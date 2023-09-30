afd84179b700f1d6046a242d697686d6be270193
Artenis67 <artenis67eininger@gmail.com>
2023-09-30 11:49:51 +0200

    Mini Patch

--- wforce_python.py
+++ wforce_python.py
@@ -7,17 +7,7 @@ driver = webdriver.Chrome()  # Assurez-vous d'avoir installé ChromeDriver et qu
 driver.get("https://svt-erlich.fr/seconde/enseignement-obligatoire/theme-2-la-terre-la-vie-et-lorganisation-du-vivant/")
 
 # Attendre que la page se charge
-time.sleep(5)
-
-# Cliquez sur l'élément spécifique (ajustez le sélecteur CSS en conséquence)
-try:
-    element = driver.find_element_by_css_selector('votre_selecteur_css')  # Remplacez "votre_selecteur_css" par le sélecteur approprié
-    element.click()
-except Exception as e:
-    print("Erreur lors de la recherche de l'élément :", e)
-
-# Attendez un court instant
-time.sleep(1)
+time.sleep(10)
 
 # Générez tous les chiffres
 def generer_tous_les_chiffres():
@@ -26,13 +16,15 @@ def generer_tous_les_chiffres():
 
 chiffres = generer_tous_les_chiffres()
 
-# Boucle pour envoyer chaque chiffre à un emplacement spécifique sur la page
+# Ciblez l'élément input par son attribut placeholder
+champ_saisie = driver.find_element_by_css_selector('input[placeholder="Mot de passe donné par votre enseignants value data dashlane-rid-case38758472"]')
+
+# Boucle pour envoyer chaque chiffre à l'élément input
 for chiffre in chiffres:
     # Copiez le chiffre dans le presse-papiers (en utilisant JavaScript)
     driver.execute_script(f"navigator.clipboard.writeText('{chiffre}')")
 
-    # Collez le chiffre dans un champ de saisie (ajustez l'élément selon votre page)
-    champ_saisie = driver.find_element_by_id("champ")  # Remplacez "champ" par l'ID de votre champ
+    # Collez le chiffre dans l'élément input
     champ_saisie.clear()  # Effacez le champ s'il contient déjà du texte
     champ_saisie.send_keys(Keys.CONTROL, 'v')
 
